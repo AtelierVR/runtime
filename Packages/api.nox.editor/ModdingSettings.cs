3539825d3ea09b6d0ffb1dfb29b558bde5c8b489
hactazia <hactazia@gmail.com>
2025-11-13 05:29:01 +0100

    refactor: implement dynamic scripting backend selection in ModdingSettings

--- Packages/api.nox.editor/ModdingSettings.cs
+++ Packages/api.nox.editor/ModdingSettings.cs
@@ -5,13 +5,50 @@ using UnityEditor.Build;
 
 namespace Nox.Editor {
 	public class ModdingSettings {
+		public static ScriptingImplementation Implementation {
+			get
+				=> Config.LoadEditor()
+						.Get("scripting_backend", "mono")
+						.ToLowerInvariant()
+					switch {
+						"il2cpp" => ScriptingImplementation.IL2CPP,
+						_        => ScriptingImplementation.Mono2x
+					};
+			set {
+				var val = value switch {
+					ScriptingImplementation.IL2CPP => "il2cpp",
+					_                              => "mono"
+				};
+				var config = Config.LoadEditor();
+				config.Set("scripting_backend", val);
+				config.Save();
+			}
+		}
+
+		[MenuItem("Nox/Other/Set Scripting Backend to Mono")]
+		public static void SetScriptingBackendToMono() {
+			Implementation = ScriptingImplementation.Mono2x;
+			Init();
+			Logger.Log("Scripting backend set to Mono.");
+		}
+		
+		[MenuItem("Nox/Other/Set Scripting Backend to IL2CPP")]
+		public static void SetScriptingBackendToIL2CPP() {
+			Implementation = ScriptingImplementation.IL2CPP;
+			Init();
+			Logger.Log("Scripting backend set to IL2CPP.");
+		}
+		
 		[InitializeOnLoadMethod]
 		public static void Init() {
 			ScriptingDefinitions.Add("NOX_SDK", NamedBuildTarget.Standalone);
 			PlayerSettings.stripEngineCode = false;
-			PlayerSettings.SetScriptingBackend(NamedBuildTarget.Standalone, ScriptingImplementation.IL2CPP);
+
+			PlayerSettings.SetScriptingBackend(NamedBuildTarget.Standalone, Implementation);
+			
 			PlayerSettings.SetApiCompatibilityLevel(NamedBuildTarget.Standalone, ApiCompatibilityLevel.NET_Standard_2_0);
 			PlayerSettings.SetEditorAssembliesCompatibilityLevel(EditorAssembliesCompatibilityLevel.NET_Standard);
+
 			PlayerSettings.insecureHttpOption = InsecureHttpOption.AlwaysAllowed;
 			ModLinkerHelper.EnsureLinkerClassExists();
 		}
