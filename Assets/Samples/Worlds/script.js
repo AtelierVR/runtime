c24deff350a5ff48e94220b88b009b352e021071
hactazia <hactazia@gmail.com>
2025-11-13 05:32:03 +0100

    refactor: update DemoWorld and script.js for target handling and position reset logic

--- Assets/Samples/Worlds/script.js
+++ Assets/Samples/Worlds/script.js
@@ -1,10 +1,33 @@
-import { transform } from 'behaviour';
+import { transform, rigidbody } from 'behaviour';
+import { log } from 'console';
 
 let d = Date.now();
 export let speed = 0.1;
+
+export let exports = {
+    target: null,
+    distance: 10
+};
+
+export function onAwake() {
+    log("Script Awake");
+    reset();
+}
+
+function reset() {
+    if (!exports?.target) return;
+    transform.position = exports.target.position;
+    if (!rigidbody) return;
+    rigidbody.linearVelocity = new Vector3(0, 0, 0);
+    rigidbody.angularVelocity = new Vector3(0, 0, 0);
+}
 
 export function onUpdate() {
     let delta = Date.now() - d;
     d = Date.now();
     transform.Rotate(delta * -speed, delta * speed, delta * -speed);
+
+    if (!exports?.target) return;
+    var dis = Vector3.Distance(transform.position, exports.target.position);
+    if (dis > exports.distance) reset();
 }
