c0d8a1037dc918f0f785c0820e9f5443860d3ecf
Hactazia <hactazia@gmail.com>
2026-05-25 12:58:22 +0200

    chore: update submodule refs and remove api.nox.player

--- Packages/api.nox.player/sdk/IPlayerAPI.cs
+++
@@ -1,7 +0,0 @@
-namespace Nox.Players {
-	/// <summary>
-	/// Interface for the player API, allowing to manage players in the game.
-	/// </summary>
-	public interface IPlayerAPI {
-	}
-}
--- Packages/api.nox.player/sdk/PlayerPhysical.cs
+++
@@ -1,6 +0,0 @@
-using Nox.Avatars;
-using Nox.Entities;
-
-namespace Nox.Players {
-	public abstract class PlayerPhysical : Physical { }
-}
